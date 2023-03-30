fc05db14edffb33c7174ccec910cf8fc5ed42005
Abu Abdullah <abdullah72308@gmail.com>
2023-03-31 00:12:10 +0600

    blog card ui added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+import Blogs from './Component/Blog/Blogs';
 import Header from './Component/Header/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
   return (
-    <div className="App">
+    <div className="App container">
       <Header></Header>
+      <Blogs></Blogs>
       
     </div>
   )
